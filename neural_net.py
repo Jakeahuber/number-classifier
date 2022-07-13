c4dc40c7e0bcfa6c98ad28c52810ea53cb9402e0
Jacob Huber <jakealanhuber@gmail.com>
2022-07-13 15:30:59 -0400

    Machine learning model now predicts numbers with 91% accuracy after 10 epochs

--- neural_net.py
+++ neural_net.py
@@ -6,11 +6,11 @@ class Neural_Net(nn.Module):
     # Constructor
     def __init__(self):
         # Allows access to methods within nn.Module
-        super(Neural_Net, self).__init__()
+        super(Neural_Net, self).__init__()      
 
         # 784 - size of each input sample (image of size 28x28)
         # 1 - size of each output sample (number 0-9)
-        self.linear = nn.Linear(28, 1, bias=True)
+        self.linear = nn.Linear(28 * 28, 10, bias=True)
 
     def forward(self, x):
         x = torch.sigmoid(self.linear(x))
--- number_classifier.py
+++ number_classifier.py
@@ -1,16 +1,14 @@
-import torch, torchvision
+import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
-from mnist import MNIST
 from neural_net import Neural_Net
 from torchvision import datasets
 from torchvision.transforms import ToTensor
     
-num_epochs = 2
-learning_rate = 0.01
+num_epochs = 10
+learning_rate = 0.001
 momentum = 0.9
-batch_size = 1 
+batch_size = 1
 
 # Create datasets for training & validation
 # There are 60,000 images in the training set. Each one is 28x28, or 784 pixels.
@@ -24,7 +22,7 @@ training_set = datasets.MNIST(
 # Why do i shuffle the training loader?
 training_loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size, shuffle=True)
 
-# There are 10,000 items in the test set.
+# There are 10,000 items in the testing set.
 testing_set = datasets.MNIST(
     root='training-set/',
     train=False,
@@ -46,6 +44,8 @@ for epoch in range(num_epochs): # loop over training set num_epoch times
     running_loss = 0.0
     for i, data in enumerate(training_loader, 0): # Problem here...
         inputs, labels = data
+        inputs = inputs.view(-1, 28*28).requires_grad_()
+
         optimizer.zero_grad()
         outputs = neural_net(inputs)
         loss = criterion(outputs, labels)
@@ -54,13 +54,13 @@ for epoch in range(num_epochs): # loop over training set num_epoch times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % 1000 == 999:    # print every 2000 mini-batches
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
 
+PATH = './model.pth'
+torch.save(neural_net.state_dict(), PATH)
 print('Finished Training')
-
-
 
 
 
